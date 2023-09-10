c1c717f3fe81c45bb9bcf38b6bb66fb750481617
SurajSSingh <22825536+SurajSSingh@users.noreply.github.com>
2023-09-09 20:50:05 -0700

    Add some alerts and change title

--- src/App.svelte
+++ src/App.svelte
@@ -25,20 +25,27 @@
   function testNotification(){
     if(canNotify){
       const notification = new Notification("This is a test!");
+      alert(`'${notification.title}' has run!`)
     }
   }
 
   function delayedTestNotification(){
     setTimeout(testNotification, numberOfSecond * 1000);
   }
+
+  function alertedTestNotification(){
+    alert("Clicked Test");
+    setTimeout(testNotification, numberOfSecond * 1000);
+  }
 </script>
 
 <main>
-<h1>Simple Pomodoro Timer</h1>
+<h1>Simple Notification Tester</h1>
 {#if !canNotify}
 <button on:click={getNotify}>Allow Notification</button>
 {:else}
 <button on:click={testNotification}>Test Notify</button>
 <button on:click={delayedTestNotification}>Test Delayed Notify</button>
+<button on:click={alertedTestNotification}>Test Alert Notify</button>
 {/if}
 </main>
