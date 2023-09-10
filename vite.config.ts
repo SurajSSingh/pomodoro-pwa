08d02b1b031b52fa3105580dfb346f82c2f6f256
SurajSSingh <22825536+SurajSSingh@users.noreply.github.com>
2023-09-09 19:59:33 -0700

    Remove Base

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,4 @@ import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte(),VitePWA({ registerType: 'autoUpdate' })],
-  base: "/pomodoro-pwa/",
 })
