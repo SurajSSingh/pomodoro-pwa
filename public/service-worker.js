72301fb1d56224562d5b8b2e24fc9e65c21fe463
SurajSSingh <22825536+SurajSSingh@users.noreply.github.com>
2023-09-10 18:52:52 -0700

    Add Service Worker register

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,10 +1,6 @@
 // service-worker.js
 self.addEventListener('fetch', (event) => {
-    // Custom logic for handling fetch events
-    // ...
-  
-    // Example of calling showNotification()
     self.registration.showNotification('Hello', {
-      body: 'This is a notification',
+      body: 'This is a test notification',
     });
   });
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import './app.css'
 import App from './App.svelte'
+import { register } from 'register-service-worker';
 
+register('/service-worker.js', {});
 const app = new App({
   target: document.getElementById('app'),
 })
